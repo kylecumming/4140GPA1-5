5f3e567e7d375a5e23d025a1ee9ea6ddb80cb0b0
Elizabeth Eddy <el278962@dal.ca>
2021-05-21 11:34:19 -0300

    Add mock data script

---
+++ mock_data_inserts.sql
@@ -0,0 +1,13 @@
+
+INSERT INTO clientUser17 VALUES (1, 'Company 1', 'Halifax', '12345', 100);
+INSERT INTO clientUser17 VALUES (2, 'Company 2', 'Halifax', 'abc', 500.30);
+
+INSERT INTO parts17 VALUES (1, 'Part 1', 'A Part', 12.5, 2);
+INSERT INTO parts17 VALUES (2, 'Part 2', 'Another Part', 10, 10);
+
+INSERT INTO POs17 VALUES (1, 2, '2021-04-03' , 'Complete');
+INSERT INTO POs17 VALUES (2, 1, '2021-05-20' , 'Pending');
+
+INSERT INTO POLines17 VALUES (1, 1, 1, 25, 2);
+INSERT INTO POLines17 VALUES (2, 1, 2, 30, 3);
+INSERT INTO POLines17 VALUES (3, 2, 2, 50, 5);
