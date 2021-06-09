6f61d453f48f70ca308d137b0a4405a0f1c80279
Takumi Kariya <tk724947@dal.ca>
2021-06-09 15:37:22 -0300

    edited company_agent_service.js to accept the request for POs

--- company_agent_service.js
+++ company_agent_service.js
@@ -32,6 +32,35 @@ app.get('/api/getPart17/:partNo17', (req, res) => {
 
 })
 
+app.get('/api/getPOs17/:poNo17', (req, res) => {
+
+    let SQL_list_one_po = 'SELECT * FROM POs17 WHERE poNo17 = ?';
+    connection.query(SQL_list_one_po, [req.params.poNo17], (error, result) => {
+        if (error) {
+            throw error
+        } else if (result.length === 0) {
+            res.status(404).send(`Error: PO with ID ${req.params.poNo17} was not found`)
+        } else {
+            res.send(result[0])
+        }
+    });
+
+});
+
+app.get('/api/getPOList', (req, res) => {
+
+    let SQL_list_POs = 'SELECT * FROM POs17';
+    connection.query(SQL_list_POs, (error, result) => {
+        if (error) {
+            throw error
+        } else if (result.length === 0) {
+            res.status(404).send(`Error: PO list is empty`)
+        } else {
+            res.send(result)
+        }
+    });
+});
+
 app.listen(3000, () => {
     console.log(`Listening on port 3000...`)
 });
