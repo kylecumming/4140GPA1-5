8c5284425814588fad5b86c5c9ebc453f06d2eb0
Elizabeth Eddy <el278962@dal.ca>
2021-07-08 12:10:35 -0300

    Add agentsPassword table

---
+++ sql/agentsPasswords_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `eeddy`.`AgentsPasswords17` (
+  `username17` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NULL,
+  PRIMARY KEY (`username17`));
+
+INSERT INTO AgentsPasswords17 (`username17`, `password`) VALUES ('abc', '123');
+INSERT INTO AgentsPasswords17 (`username17`, `password`) VALUES ('qwerty', '987654321');
