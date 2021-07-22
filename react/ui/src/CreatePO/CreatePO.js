3bdcdcaa5ed0e6107d312a3069f7dbb2018a7d2f
Elizabeth Eddy <el278962@dal.ca>
2021-07-22 15:03:32 -0300

    Fix create PO

--- react/ui/src/CreatePO/CreatePO.js
+++ react/ui/src/CreatePO/CreatePO.js
@@ -11,28 +11,30 @@ export default class CreatePO extends Component {
             PONo: '',
             CompanyID: 1,
             quantity: '',
-            password: 1
+            password: '12345',
+            createError: null,
         };
 
         this.submit = this.submit.bind(this);
+
     }
 
     submit(event) {
-        
         event.preventDefault();
         Axios.post('http://localhost:3000/api/client/postNewOrder17', {
-            'clientCompId17': this.state.companyID,
-            'partNo17': this.state.PONo,
-            'qty17': this.state.quantity,
+            'clientCompId17': parseInt(this.state.CompanyID),
+            'partNo17': parseInt(this.state.PONo),
+            'qty17': parseInt(this.state.quantity),
             'clientCompPassword17': this.state.password
+
         }).then((response) => {
             if (response.status === 200) {
                 window.location.href = "/home";
             } else {
-                alert(response.status);
+                console.log(response.status)
             }
-        }).catch(function (error) {
-            alert(error);
+        }).catch((err) => {
+            this.setState({ createError: err.response.data });
         });
     }
 
@@ -48,7 +50,7 @@ export default class CreatePO extends Component {
                     </label>
                     <input
                         id="input-box-first"
-                        placeholder="PO Number"
+                        placeholder="Part Number"
                         onChange={(e) => this.setState({ PONo: e.target.value })}
                         required
                     />
@@ -60,6 +62,7 @@ export default class CreatePO extends Component {
                     />
                     <button type="submit" >Create</button>
                 </form>
+                {this.state.createError !== null && <h5 style={{ margin: '10px', color: 'red' }}> {this.state.createError} </h5>}
             </div >
         );
     }
